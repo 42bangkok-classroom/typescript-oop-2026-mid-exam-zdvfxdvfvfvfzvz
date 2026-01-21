0ec7ecec558941d564b6c53147795c65c3bd2fee
zdvfxdvfvfvfzvz <68010756@kmitl.ac.th>
2026-01-21 14:57:15 +0700

    push4

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,21 @@
 // Write your code below
+function fizzbuzz(num: number) {
+    if (num <= 0) {
+        return 0;
+    }
+    if (num % 15 === 0) {
+        return "FizzBuzz";
+    }
+    if (num % 3 === 0) {
+        return "Fizz";
+    }
+    if (num % 5 === 0) {
+        return "Buzz";
+    } else {
+        return num;
+    }
+}
+
+let strr: string = (process.argv.slice(2).toString());
+let nume = +strr;
+console.log(fizzbuzz(nume));
