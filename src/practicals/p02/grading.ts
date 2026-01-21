d7b065f64dd658448b286a00de5df69e724081d1
zdvfxdvfvfvfzvz <68010756@kmitl.ac.th>
2026-01-21 14:44:06 +0700

    push3

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,28 @@
 // Write your code below
+
+function grading(grade: number) {
+    if (grade > 100 || grade < 0) {
+        return "Invalid input"
+    }
+    if (grade <= 49) {
+        return "F";
+    }
+    if (grade <= 59) {
+        return "D";
+    }
+    if (grade <= 69) {
+        return "C";
+    }
+    if (grade <= 79) {
+        return "B";
+    }
+    if (grade <= 100) {
+        return "A";
+    } else {
+        return "Invalid input";
+    }
+}
+
+let str: string = (process.argv.slice(2).toString());
+let num = +str;
+console.log(grading(num));
